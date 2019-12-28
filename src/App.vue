807cfe883b05c5ac929f32c32311c1565fe201ae
Yusuf Borucu <yusufborucu@gmail.com>
2019-12-28 15:06:50 +0300

    last commit

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,77 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div>
+    <div class="post">
+      <!-- vue2-editor'ü ekrana yerleştirdik ve v-model ile post değişkenine bağladık. -->
+      <vue-editor v-model="post"></vue-editor>
+      <br>
+      <!-- Editöre yazılan yazının çıktısını gösterip gizlemek için bir buton koyduk. 
+           Butona basılınca show metoduna gitmesini söyledik. -->      
+      <button @click="show">Göster</button>
+    </div>
+    <!-- Editöre yazılan yazının çıktısını göstermek için bir div açtık. 
+         Bu div'e main.js dosyasında tanımlamış olduğumuz highlightjs directive'ini verdik.
+         v-model ile post değişkenine bağladık.
+         showButton değişkenine göre gösterip gizlemek için v-if kullandık. -->
+    <div class="content" v-highlightjs v-html="post" v-if="showButton"></div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  import { VueEditor } from "vue2-editor";
+
+  export default {
+    // import ettiğimiz vue2-editor'ü bir component olarak belirttik.
+    components: {
+			VueEditor
+    },
+    data() {
+      return {
+        post: "",
+        showButton: false
+      }
+    },
+    methods: {
+      /* Göster butonuna tıklayınca çalışacak olan bu fonksiyonda, 
+         editörde yazılan yazıda bulunan kod kısımlarına (pre tag'ı içinde bulunan alanlar)      
+         replace ederek o kısımlara <code> tag'ı ekledik.
+      */
+      show() {
+        var find = ['<pre class="ql-syntax" spellcheck="false">', '</pre>'];
+				var replace = ['<pre class="ql-syntax" spellcheck="false"><code>', '</code></pre>'];
+				this.post = this.replaceArray(this.post, find, replace);
+        this.showButton = !this.showButton;        
+      },
+      // bir string içerisinde birden fazla replace işlemi yapmak için kullanılan fonksiyon
+      replaceArray(text, find, replace) {
+				var replaceString = text;
+				var regex; 
+				for (var i = 0; i < find.length; i++) {
+					regex = new RegExp(find[i], "g");
+					replaceString = replaceString.replace(regex, replace[i]);
+				}
+				return replaceString;
+			}
     }
   }
-}
 </script>
 
+<!-- Ufak stil düzenlemeleri yaptık. -->
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+  .post {
+    width: 850px;
+    text-align: center;
+  }
+  .post button {
+    padding: 10px;
+    font-size: 15px;
+  }
+  .content {
+    margin-top: 20px;
+    padding: 10px;
+    width: 850px;
+    border: 1px solid #ff0000;
+  }
+  .ql-snow .ql-editor code {
+    background-color: #23241f!important;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import hljs from 'highlight.js';
+Vue.directive('highlightjs', {
+  deep: true,
+  bind: function (el, binding) {
+    let targets = el.querySelectorAll('code');
+    targets.forEach((target) => {
+      if (binding.value) {
+        target.innerHTML = binding.value;
+      }
+      hljs.highlightBlock(target);
+    });
+  }
+});
 
 new Vue({
   el: '#app',
   render: h => h(App)
-})
+});
